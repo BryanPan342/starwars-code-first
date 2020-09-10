9efb83798f9127423edc0f02d6a21fd87b8dd0ab
Bryan Pan <bryanpan342@gmail.com>
2020-09-10 16:40:07 -0700

    clean up and make dependent on 1.62.0

--- lib/fine-grain-implementation/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './global-types';
-export * from './object-types/film';
-export * from './object-types/species';
-export * from './object-types/person';
-export * from './object-types/planet';
-export * from './object-types/starship';
-export * from './object-types/vehicle';
